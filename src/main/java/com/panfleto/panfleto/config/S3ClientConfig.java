bcf4bf66e259639f5a98447c9ffa2e30e0c2d546
ernany <julioernany@gmail.com>
2024-08-12 00:46:22 -0400

    S3ClientConfig

---
+++ src/main/java/com/panfleto/panfleto/config/S3ClientConfig.java
@@ -0,0 +1,32 @@
+package com.panfleto.panfleto.config;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+public class S3ClientConfig {
+
+    @Bean
+    public S3Client createS3Client() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+                System.getenv("AWS_ACESSKEY"),
+                System.getenv("AWS_SECRETKEY")
+        );
+
+        return S3Client.builder()
+                .endpointOverride(URI.create(System.getenv("R2_ENDPOINT")))
+                .region(Region.of("auto"))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
+                .build();
+    }
+}
